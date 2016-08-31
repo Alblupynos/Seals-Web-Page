a31b5100c425ac3cd81a351e2840382436f9858d
Albina <alblupynos@gmail.com>
2016-08-31 21:44:23 +0300

    add comments using angular ng-repeat

--- script/app.js
+++ script/app.js
@@ -1,6 +1,19 @@
-(function(){
-  var app = angular.module("seal", []);
-})();
+var model = {
+    items: [
+        { name: "Chandler Bing", comment: "They are so cute!"},
+        { name: "John Doe", comment: "One word... WOW!!"},
+        { name: "Chandler Bing", comment: "I love seals!"}
+    ]
+};
+var sealApp = angular.module("seal", []);
+sealApp.controller("CommentsController", function ($scope) {
+$scope.list = model;
+$scope.addComment = function(name, comment) {
+  if(name != "" && comment != "") {
+    $scope.list.items.push({name: name, comment: comment});
+  }
+}   
+});
 
 /*
 angular
